c352609471aa284d1559ca12dd8a6dbe0b4edd00
sandra-louchart <59296099+Sandra-Louchart@users.noreply.github.com>
2020-09-28 11:16:20 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+$filmographyIndiana = [
+  "The_Last_Crusade" => 1989,
+  "Indiana_Jones_And_The_Temple_Of_Doom" => 1984,
+  "The_Kingdom_Of_The_Crystal_Skull" => 2008
+];
+asort($filmographyIndiana);
+foreach ($filmographyIndiana as $key =>$filmographyIndiana){
+    echo $key;
+    echo "   ";
+    echo $filmographyIndiana ."<br>";
+    echo "   ";
+}
+?>
