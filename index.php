a895da662744574e241767e1a3d0da5d1dc3daa1
sandra-louchart <59296099+Sandra-Louchart@users.noreply.github.com>
2020-09-29 20:01:11 +0200

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ $filmographyIndiana = [
   "Indiana_Jones_And_The_Temple_Of_Doom" => 1984,
   "The_Kingdom_Of_The_Crystal_Skull" => 2008
 ];
-asort($filmographyIndiana);
+ksort($filmographyIndiana);
 foreach ($filmographyIndiana as $key =>$filmographyIndiana){
     echo $key;
     echo "   ";
